287d8979bb793792b4ce1eca873abbbf00fdbe7e
gigachurka <145013182+gigachurka@users.noreply.github.com>
2025-04-26 02:14:07 +0300

    Updated the controller, added a Razor page, updated the navigation.

--- YouTubeApi/Controllers/YouTubeController.cs
+++ YouTubeApi/Controllers/YouTubeController.cs
@@ -2,8 +2,9 @@
 using Google.Apis.YouTube.v3;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using YouTubeApi.Data;
 using YouTubeApi.Models;
-
+using Microsoft.EntityFrameworkCore;
 namespace YouTubeApi.Controllers
 {
     /// <summary>
@@ -14,6 +15,13 @@ namespace YouTubeApi.Controllers
     [ApiController]
     public class YouTubeController : Controller
     {
+
+        public YouTubeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private readonly ApplicationDbContext _context;
         [HttpGet]
         public async Task<IActionResult> GetChannelsVideos(string? pageToken = null, int maxResults = 50)
         {
@@ -41,8 +49,37 @@ namespace YouTubeApi.Controllers
                NextPageToken = searchResponse.NextPageToken,
                PrevPageToken = searchResponse.PrevPageToken
            };
+
+            foreach (var item in searchResponse.Items)
+            {
+                var video = new VideoEntity
+                {
+                    Title = item.Snippet.Title,
+                    Link = $"https://www.youtube.com/watch?v={item.Id.VideoId}",
+                    Thumbnail = item.Snippet.Thumbnails.Medium.Url,
+                    PublishedAt = item.Snippet.PublishedAtDateTimeOffset
+                };
+
+                if (!_context.Videos.Any(v => v.Link == video.Link))
+                {
+                    _context.Videos.Add(video);
+                }
+            }
+            await _context.SaveChangesAsync();
+
+
             return Ok(response);
         }
+        [HttpGet("saved")]
+        public async Task<IActionResult> GetSavedVideos()
+        {
+            var videos = await _context.Videos
+                .OrderByDescending(v => v.PublishedAt)
+                .ToListAsync();
+
+            return Ok(videos);
+        }
+
     }
 }
 
--- YouTubeApi/Program.cs
+++ YouTubeApi/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using YouTubeApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -17,9 +23,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();

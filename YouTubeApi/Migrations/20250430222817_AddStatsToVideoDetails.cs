8a40932bbf81aae2eac5a1003a63c90e137aafad
gigachurka <145013182+gigachurka@users.noreply.github.com>
2025-05-01 04:05:47 +0300

    added the number of views, likes, comments, updated the table in the database

---
+++ YouTubeApi/Migrations/20250430222817_AddStatsToVideoDetails.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YouTubeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatsToVideoDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CommentCount",
+                table: "Videos",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LikeCount",
+                table: "Videos",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ViewCount",
+                table: "Videos",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentCount",
+                table: "Videos");
+
+            migrationBuilder.DropColumn(
+                name: "LikeCount",
+                table: "Videos");
+
+            migrationBuilder.DropColumn(
+                name: "ViewCount",
+                table: "Videos");
+        }
+    }
+}
